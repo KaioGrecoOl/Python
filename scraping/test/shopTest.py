b44125f177f625b7cc5fc64813bbfff8c0bd29f7
Kaio G <kaiogreco91@gmail.com>
2022-11-18 16:32:31 -0300

    feat: :zap: children
    entendendo a funcionalidade children, que encontra somente os descendentes que sejam filhos

---
+++ scraping/test/shopTest.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = urlopen("https://pythonscraping.com/pages/page3.html")
+bs = BeautifulSoup(url, 'html.parser')
+
+for child in bs.find('table',{'id':'giftList'}).children:
+    print(child)
