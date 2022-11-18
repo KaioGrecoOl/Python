92de7083e3d65c96bfbb214c6cebaf8851af3494
Kaio G <kaiogreco91@gmail.com>
2022-11-18 17:33:48 -0300

    feat: next_sibiling

--- scraping/test/shopTest.py
+++ scraping/test/shopTest.py
@@ -4,5 +4,8 @@ from bs4 import BeautifulSoup
 url = urlopen("https://pythonscraping.com/pages/page3.html")
 bs = BeautifulSoup(url, 'html.parser')
 
-for child in bs.find('table',{'id':'giftList'}).children:
-    print(child)
+# for child in bs.find('table',{'id':'giftList'}).children:
+#     print(child)
+
+for sibiling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+    print(sibiling)
