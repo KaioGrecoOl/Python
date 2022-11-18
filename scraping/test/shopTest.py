397f42e06c71b3b12bcd604e6fa3f66a789ac19a
Kaio G <kaiogreco91@gmail.com>
2022-11-18 17:38:36 -0300

    feat: parent

--- scraping/test/shopTest.py
+++ scraping/test/shopTest.py
@@ -4,8 +4,12 @@ from bs4 import BeautifulSoup
 url = urlopen("https://pythonscraping.com/pages/page3.html")
 bs = BeautifulSoup(url, 'html.parser')
 
+print(bs.find('img',
+              {'src':'../img/gifts/img1.jpg'})
+      .parent.previous_sibling.get_text())
+
 # for child in bs.find('table',{'id':'giftList'}).children:
 #     print(child)
 
-for sibiling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
-    print(sibiling)
+# for sibiling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+#     print(sibiling)
