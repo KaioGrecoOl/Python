6c041f7141e56c178079f396339e30b0cbbdd068
Kaio G <kaiogreco91@gmail.com>
2022-11-18 16:14:57 -0300

    feat: findAll
    realizando busca com base no argumento text

--- scraping/test/warandpeace.py
+++ scraping/test/warandpeace.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup
 url = urlopen("https://pythonscraping.com/pages/warandpeace.html")
 bs = BeautifulSoup(url.read(), 'html.parser')
 
-nameList = bs.findAll("span", {"class": {"green", "red"}})
+nameList = bs.findAll(text="the prince")
 
-for name in nameList:
-  print(name.get_text())
+print(nameList)
+
+# for name in nameList:
+#   print(name.get_text())
