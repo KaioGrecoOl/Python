dd3a2ce2353e1ca1f0315c416c02975072adac70
Kaio G <kaiogreco91@gmail.com>
2022-11-18 16:01:46 -0300

    build: findAll
    realizando nova busca no findAll para localizar novos elementos com base em um atributos passado como dicionário

--- scraping/test/warandpeace.py
+++ scraping/test/warandpeace.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = urlopen("https://pythonscraping.com/pages/warandpeace.html")
 bs = BeautifulSoup(url.read(), 'html.parser')
 
-nameList = bs.findAll("span", {"class": "green"})
+nameList = bs.findAll("span", {"class": {"green", "red"}})
 
 for name in nameList:
   print(name.get_text())
