dd909eca531e0f223d9e9a818f67d5b12b268907
Kaio G <kaiogreco91@gmail.com>
2022-11-18 15:56:03 -0300

    feat: :zap: findAll
    realizando busca de nomes próprios no texto de acordo com as tags

--- scraping/test/warandpeace.py
+++ scraping/test/warandpeace.py
@@ -4,4 +4,7 @@ from bs4 import BeautifulSoup
 url = urlopen("https://pythonscraping.com/pages/warandpeace.html")
 bs = BeautifulSoup(url.read(), 'html.parser')
 
-print(bs.text)
+nameList = bs.findAll("span", {"class": "green"})
+
+for name in nameList:
+  print(name.get_text())
