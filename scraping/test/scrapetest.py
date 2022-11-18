c4c0f0c04fc590ff3b3e336a3d9fe9a3863952fa
Kaio G <kaiogreco91@gmail.com>
2022-11-18 15:21:59 -0300

    feat: beautifulsoup
    realizando primeira busca com o beautifulsoup de uma tag html

--- scraping/test/scrapetest.py
+++ scraping/test/scrapetest.py
@@ -1,7 +1,17 @@
-import urllib.request
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-html = urllib.request.urlopen("https://pythonscraping.com/pages/page1.html")
 
-text = html.read().decode("utf8")
+url = urlopen("https://pythonscraping.com/pages/page1.html")
+bs = BeautifulSoup(url.read(), 'html.parser')
 
-print(text)
+print(bs.h1)
+
+
+# import urllib.request
+
+# html = urllib.request.urlopen("https://pythonscraping.com/pages/page1.html")
+
+# text = html.read().decode("utf8")
+
+# print(text)
