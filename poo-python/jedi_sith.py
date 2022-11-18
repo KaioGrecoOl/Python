d49ff5fcd2080aeab390540644a9e3b9c2def002
Kaio G <kaiogreco91@gmail.com>
2022-11-17 22:11:51 -0300

    feat: cria os métodos attack, defense

--- poo-python/jedi_sith.py
+++ poo-python/jedi_sith.py
@@ -1,15 +1,25 @@
 from character import Character
-
-class Jedi(Character):
-  def __init__(self, name, specy, weight, height, hp):
-      super().__init__(name, specy, weight, height, hp)
-      self.lightsaber = "Green lightsaber"
+from random import choice
+from light_saber import LightSaber
 
 
 class Sith(Character):
   def __init__(self, name, specy, weight, height, hp):
       super().__init__(name, specy, weight, height, hp)
-      self.lightsaber = "Red lightsaber"
+      self.lightsaber = LightSaber("Red", 40)
 
   def attack(self, character):
-    character.set_hp(10)
+    if not character.defense():
+        character.set_hp(self.lightsaber.force)
+
+  class Jedi(Character):
+    def __init__(self, name, specy, weight, height, hp):
+        super().__init__(name, specy, weight, height, hp)
+        self.lightsaber = LightSaber("Green", 20)
+
+
+    def defense(self):
+      defe = choice([True, False])
+      if defe:
+        print(f"{self.name} defense")
+      return defe
