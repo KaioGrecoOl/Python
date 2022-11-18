91de68616fd00b7bdc517f19ea74c8a8d02acdea
Kaio G <kaiogreco91@gmail.com>
2022-11-18 15:45:09 -0300

    refactor: :art: getTitle
    criamos função que realiza extração do titulo da página com exceção de erro

--- scraping/test/scrapetest.py
+++ scraping/test/scrapetest.py
@@ -1,28 +1,22 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from urllib.error import URLError
 from bs4 import BeautifulSoup
 
+def getTitle(url):
+  try:
+    url = urlopen(url)
+  except HTTPError as e:
+    return None
+  try:
+    bs = BeautifulSoup(url.read(), 'html.parser')
+    title = bs.body.h1 
+  except AttributeError as e:
+    return None
+  return title
 
-try:
-  url = urlopen("https://pythonscraping.com/pages/page1.html")
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print("The server could not be found!")
+
+title = getTitle("https://pythonscraping.com/pages/page1.html")
+if title == None:
+  print("Title could not be found")
 else:
-    print("It worked!")
-
-    
-# bs = BeautifulSoup(url.read(), 'html.parser')
-
-# print(bs.h1)
-
-
-# import urllib.request
-
-# html = urllib.request.urlopen("https://pythonscraping.com/pages/page1.html")
-
-# text = html.read().decode("utf8")
-
-# print(text)
+  print(title)
