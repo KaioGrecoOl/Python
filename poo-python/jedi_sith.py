0279741f63cbaaa65a5ba9095e94c32884237029
Kaio G <kaiogreco91@gmail.com>
2022-11-17 21:28:01 -0300

    build: :zap: adicionando atributos especiais das subclasses

--- poo-python/jedi_sith.py
+++ poo-python/jedi_sith.py
@@ -2,9 +2,11 @@ from character import Character
 
 class Jedi(Character):
   def __init__(self, name, specy, weight, height, hp):
-      super().init(name, specy, weight, height, hp)
+      super().__init__(name, specy, weight, height, hp)
+      self.lightsaber = "Green lightsaber"
 
 
 class Sith(Character):
   def __init__(self, name, specy, weight, height, hp):
-      super().init(name, specy, weight, height, hp)
+      super().__init__(name, specy, weight, height, hp)
+      self.lightsaber = "Red lightsaber"
