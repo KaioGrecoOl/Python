bf0f4589d1d32e211143af7733eb2cc11b360ee6
Kaio G <kaiogreco91@gmail.com>
2022-11-22 21:09:31 -0300

    feat: getAllExternalLinks
    função que coleta os links internos e externos de uma página no wikipedia

--- scraping/test/wikipediaCrowler.py
+++ scraping/test/wikipediaCrowler.py
@@ -52,3 +52,29 @@ def followExternalOnly(startingSite):
     followExternalOnly(externalLink)
             
 followExternalOnly('http://oreilly.com')
+
+# Collects a list of all external URLs found on the site
+allExtLinks = set()
+allIntLinks = set()
+
+
+def getAllExternalLinks(siteUrl):
+    html = urlopen(siteUrl)
+    domain = '{}://{}'.format(urlparse(siteUrl).scheme,
+                              urlparse(siteUrl).netloc)
+    bs = BeautifulSoup(html, 'html.parser')
+    internalLinks = getInternalLinks(bs, domain)
+    externalLinks = getExternalLinks(bs, domain)
+
+    for link in externalLinks:
+        if link not in allExtLinks:
+            allExtLinks.add(link)
+            print(link)
+    for link in internalLinks:
+        if link not in allIntLinks:
+            allIntLinks.add(link)
+            getAllExternalLinks(link)
+
+
+allIntLinks.add('http://oreilly.com')
+getAllExternalLinks('http://oreilly.com')
